5ad6b02f737abf2f5ee7e13db05f7b44074a54a8
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-27 15:00:49 -0400

    created gol configuration

---
+++ configurations.py
@@ -0,0 +1,26 @@
+class Configuration:
+
+    def __init__(self):
+        self.alive_cells = set()
+
+    def set_cell(self, pos):
+        self.alive_cells.add(pos)
+
+    def clear_cell(self, pos):
+        if pos in self.alive_cells:
+            self.alive_cells.remove(pos)
+
+    def set_cells(self, pos_arr):
+        [self.set_cell(pos) for pos in pos_arr]
+
+    def clear_cells(self, pos_arr):
+        [self.clear_cell(pos) for pos in pos_arr]
+
+    def is_set(self, pos):
+        return pos in self.alive_cells
+
+    def place(self, config, loc=(0, 0)):
+        self.set_cells([(cell[0] + loc[0], cell[1] + loc[1]) for cell in config.alive_cells])
+
+    def render(self):
+        print("State:", self.alive_cells)
