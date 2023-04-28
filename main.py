55ce3c0d7e1682c8a937708f32845d7a573052a0
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-28 01:33:13 -0400

    updated file path

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pygame.init()
 def initialize_game_dev(g: GameOfLife):
     g.clear()
 
-    g.load("./rendal-attic/memcell.txt")
+    g.load("./configs/rendal-attic/memcell.txt")
 
     return g
 
