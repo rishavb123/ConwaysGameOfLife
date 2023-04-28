ac1d2aca3be705d2a951327febdeb9e88eaffe84
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-28 16:04:56 -0400

    added flip functions

--- configurations.py
+++ configurations.py
@@ -87,6 +87,14 @@ class Configuration:
     
     def rotate_ccw(self):
         return self.rotate_cw().rotate_cw().rotate_cw()
+    
+    def flip_x(self):
+        self.alive_cells = set((-cell[0], cell[1]) for cell in self.alive_cells)
+        return self
+    
+    def flip_y(self):
+        self.alive_cells = set((cell[0], -cell[1]) for cell in self.alive_cells)
+        return self
 
     def copy(self):
         c = self.__class__()
