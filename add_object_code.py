4a80cfe1314ee1464fed68b6800c5d9beeba3e28
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-27 23:59:37 -0400

    added lexicon functionality

--- add_object_code.py
+++ add_object_code.py
@@ -4,7 +4,8 @@ from configurations import Configuration
 def make_parser():
     parser = argparse.ArgumentParser(description="Add serialized object code to objects.py")
 
-    parser.add_argument("--file-name", "-f", type=str, help="The serialized object file.")
+    parser.add_argument("--file-name", "-f", default=None, type=str, help="The serialized object file.")
+    parser.add_argument("--lexicon-name", "-l", default=None, type=str, help="The name of the object from the lexicon")
     parser.add_argument("--object-name", "-o", type=str, help="The object name to use in the code.")
 
     return parser
@@ -13,10 +14,15 @@ def main():
     args = make_parser().parse_args()
 
     fname = args.file_name
+    lname = args.lexicon_name
     oname = args.object_name
 
     c = Configuration()
-    c.load(fname)
+    if fname is not None:
+        c.load(fname)
+    else:
+        c.load_from_lexicon(lname)
+    c.shift_to_origin()
 
     with open("./objects.py", "a") as f:
         arr = ",\n        ".join(f"({cell[0]}, {cell[1]})" for cell in sorted(c.alive_cells))
