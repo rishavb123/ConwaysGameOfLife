e000b52c82f601cba7577ee4316972da440fa6a8
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-28 16:18:14 -0400

    added show controls

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ def initialize_game_dev(g: GameOfLife):
     g.clear()
 
     # g.load("https://copy.sh/life/examples/101.rle")
-    g.place(GliderGun)
+    
+    g.place(GliderGun(), loc=(0, 0))
+    g.place(GliderGun().flip_over_x(), loc=(0, 30))
+
+
     # g.load('./configs/rendal-attic/memcell.txt')
+
+    # g.load('./configs/saved/double_glider.pkl')
 
     return g
 
@@ -28,7 +34,8 @@ def main(initialize_game=initialize_game_dev):
     HEIGHT = 1080
     INIT_CELL_SIZE = 19.2
     GRID_COLOR = '#222222'
-    TEXT_COLOR = '#00ff00'
+    DEBUG_COLOR = '#00ff00'
+    CONTROLS_COLOR = '#ff00ff'
     HOVER_ALIVE = '#cccccc'
     HOVER_DEAD = '#333333'
     COLOR = 'white'
@@ -36,17 +43,20 @@ def main(initialize_game=initialize_game_dev):
     FONT_SIZE = 20
     FONT = 'Courier New'
     INIT_TICK_FREQ = 3
+    MAX_FRAME_RATE = 60
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
     running = True
     dt = 0
+    generation = 0
 
     ticker = 0
     tick_period = 1 / INIT_TICK_FREQ
 
     ticking = False
     show_debug = False
+    show_controls = False
 
     ox, oy = screen.get_width() / 2, screen.get_height() / 2
 
@@ -88,6 +98,7 @@ def main(initialize_game=initialize_game_dev):
                 cell_size = new_cell_size
 
         if ticker > tick_period and ticking:
+            generation += 1
             g.tick()
             ticker = 0
 
@@ -120,16 +131,24 @@ def main(initialize_game=initialize_game_dev):
         if clicked[pygame.K_c]:
             g.clear()
             ticking = False
+            generation = 0
         if clicked[pygame.K_x]:
             initialize_game(g)
             ticking = False
+            generation = 0
         if clicked[pygame.K_g]:
             kwargs["grid_color"] = GRID_COLOR if kwargs["grid_color"] is None else None
         if clicked[pygame.K_f]:
             show_debug = not show_debug
+        if clicked[pygame.K_z]:
+            show_controls = not show_controls
 
         w = screen.get_width()
         h = screen.get_height()
+
+        mx, my = pygame.mouse.get_pos()
+        mi = int(np.floor((mx - ox) / cell_size))
+        mj = int(np.floor((my - oy) / cell_size))
 
         mouse_pressed = pygame.mouse.get_pressed()[0]
         if not mouse_old_pressed and mouse_pressed:
@@ -139,12 +158,9 @@ def main(initialize_game=initialize_game_dev):
                 int(np.floor((y - oy) / cell_size)),
             ))
         else:
-            x, y = pygame.mouse.get_pos()
-            i = int(np.floor((x - ox) / cell_size))
-            j = int(np.floor((y - oy) / cell_size))
-            sx = ox + i * cell_size
-            sy = oy + j * cell_size
-            color = HOVER_ALIVE if g.is_set((i, j)) else HOVER_DEAD
+            sx = ox + mi * cell_size
+            sy = oy + mj * cell_size
+            color = HOVER_ALIVE if g.is_set((mi, mj)) else HOVER_DEAD
             pygame.draw.rect(surface=screen, color=color, rect=(sx, sy, cell_size, cell_size), )
         mouse_old_pressed = mouse_pressed
 
@@ -169,6 +185,7 @@ def main(initialize_game=initialize_game_dev):
 
         if show_debug:
             debug_info = {
+                'generation': generation,
                 'frame_rate': f"{1/dt:0.4f}",
                 'cell_size': f"{cell_size:0.2f}",
                 'origin': f"({ox:0.2f}, {oy:0.2f})",
@@ -176,22 +193,38 @@ def main(initialize_game=initialize_game_dev):
                 'width': w,
                 'height': h,
                 'ticking': ticking,
-                'tick_freq': f"{1 / tick_period:0.4f}"
+                'tick_freq': f"{1 / tick_period:0.4f}",
+                'mouse_pos': f"({mx:0.2f}, {my:0.2f})",
+                'mouse_idx':  f"({mi:0.2f}, {mj:0.2f})",
             }
             y = 5
             s = "Debug Info:"
-            text_surface = font.render(s, True, TEXT_COLOR)
+            text_surface = font.render(s, True, DEBUG_COLOR)
             screen.blit(text_surface, dest=(5, y))
             y += FONT_SIZE
             for k, v in debug_info.items():
                 s = f"{k}: {v}"
-                text_surface = font.render(s, True, TEXT_COLOR)
+                text_surface = font.render(s, True, DEBUG_COLOR)
                 screen.blit(text_surface, dest=(2 * FONT_SIZE + 5, y))
                 y += FONT_SIZE
 
+        if show_controls:
+            controls = {
+                
+            }
+            y = h - 5 - FONT_SIZE
+            for k, v in reversed(controls.items()):
+                s = f"{k}: {v}"
+                text_surface = font.render(s, True, CONTROLS_COLOR)
+                screen.blit(text_surface, dest=(2 * FONT_SIZE + 5, y))
+                y -= FONT_SIZE
+            s = "Controls:"
+            text_surface = font.render(s, True, CONTROLS_COLOR)
+            screen.blit(text_surface, dest=(5, y))
+
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(MAX_FRAME_RATE) / 1000
 
         if ticking:
             ticker += dt
